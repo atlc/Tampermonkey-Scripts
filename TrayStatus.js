d4c6e093a62ae78ef8a0b14bed180085ed448e2f
atlc <andrewlloydcartwright+atlc@gmail.com>
2018-08-29 21:40:57 -0500

    Create TrayStatus.js

---
+++ TrayStatus.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Print tray status
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://172.16.104.76/hp/device/DeviceStatus/Index
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(window.location.reload.bind(window.location), 30000);
+
+    let tray2 = document.getElementById('TrayBinStatus_2').innerHTML;
+    let tray3 = document.getElementById('TrayBinStatus_3').innerHTML;
+
+    if (tray2 != 'OK') {
+        alert('PAPER TRAY 2 EMPTY');
+        changeFavicon();
+    }
+
+    if (tray3 != 'OK') {
+        alert('PAPER TRAY 3 EMPTY');
+        changeFavicon();
+    }
+
+    function changeFavicon() {
+        document.querySelector("link[rel='shortcut icon']").href = 'https://i.imgur.com/OZ9vGoY.png';
+    }
+})();
