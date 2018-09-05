971c9648dad68342657e9ed321f1f515a2135598
atlc <andrewlloydcartwright+atlc@gmail.com>
2018-09-05 11:04:16 -0500

    Update TrayStatus.js
    Made sure favicon is changed before hanging up on alert, increased reload frequency to 15s

--- TrayStatus.js
+++ TrayStatus.js
@@ -10,19 +10,19 @@
 
 (function() {
     'use strict';
-    setTimeout(window.location.reload.bind(window.location), 30000);
+    setTimeout(window.location.reload.bind(window.location), 15000);
 
     let tray2 = document.getElementById('TrayBinStatus_2').innerHTML;
     let tray3 = document.getElementById('TrayBinStatus_3').innerHTML;
 
     if (tray2 != 'OK') {
-        alert('PAPER TRAY 2 EMPTY');
         changeFavicon();
+        alert('PAPER TRAY 2 EMPTY');
     }
 
     if (tray3 != 'OK') {
-        alert('PAPER TRAY 3 EMPTY');
         changeFavicon();
+        alert('PAPER TRAY 3 EMPTY');
     }
 
     function changeFavicon() {
