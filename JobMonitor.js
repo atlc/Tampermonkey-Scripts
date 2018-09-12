73c8700b226f78c3e0c1dfa9ab5a0fbebea805f0
atlc <andrewlloydcartwright+atlc@gmail.com>
2018-09-12 08:18:26 -0500

    Update JobMonitor.js

--- JobMonitor.js
+++ JobMonitor.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  try to take over the world!
 // @author       You
-// @match        https://172.16.104.76/hp/device/JobLogReport/Index
+// @match        https://172.16.104.76/hp/device/JobLogReport/*
 // @grant        none
 // ==/UserScript==
 
