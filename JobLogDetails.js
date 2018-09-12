1ce46ad3880046f8c49c1a0bb78a6fd282cce780
atlc <andrewlloydcartwright+atlc@gmail.com>
2018-09-12 08:33:24 -0500

    Update JobLogDetails.js
    Bolds the page count and removes the detail fields that I don't want visible

--- JobLogDetails.js
+++ JobLogDetails.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Job Log Details
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://172.16.104.76/hp/device/JobLogReportDetails/*
@@ -13,4 +13,11 @@
     let h1 = document.querySelector('h1');
     h1.addEventListener('click', () => window.history.back());
     h1.style.border = "2px solid red";
+
+    document.getElementById('JobDetailsProperty_2').parentNode.style.fontWeight="bold";
+
+    let uselessDetails = [0,1,5,6,7,8,9,10];
+    uselessDetails.forEach((d) => {
+        document.getElementById(`JobDetailsProperty_${d}`).parentNode.remove();
+    });
 })();
