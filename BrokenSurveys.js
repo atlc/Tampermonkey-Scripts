b34fd4b76a41b95e2c0a890f1c1272f6c4cd9304
atlc <andrewlloydcartwright+atlc@gmail.com>
2018-09-14 20:43:44 -0500

    Update and rename BrokenDonalds.js to BrokenSurveys.js
    Added in other domains outside of McDonalds to show POCs

--- BrokenSurveys.js
+++ BrokenSurveys.js
@@ -1,10 +1,14 @@
 // ==UserScript==
-// @name         BrokenDonalds
+// @name         BrokenSurveys
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Proof of concept that brute forces completion of null satisfaction surveys
 // @author       You
+// @match        https://www.tellcaribou.com*
+// @match        http://www.tellamc.com/*
+// @match        https://firehouselistens.smg.com/*
 // @match        https://www.mcdvoice.com/*
+// @match        https://tellthebell.com/*
 // @grant        none
 // ==/UserScript==
 
